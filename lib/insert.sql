db5ed9b1b638459b61f65ad5d0e6a82daddaf6cd
Jeff Benton <sikletz@gmail.com>
2019-04-12 07:17:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
-INSERT INTO bears (name, age, gender, color, temperment, alive) 
+INSERT INTO bears (name, age, gender, color, temperment, alive)
+  VALUES ("Mr. Chocolate", 4, "male", "blue", "friendly", 1)
